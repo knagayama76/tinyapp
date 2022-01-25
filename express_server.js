1d378629362e3ae3a3e2dd4fa6a02f4a8846ae7a
knagayama76 <knagayama76>
2022-01-24 20:44:55 -0500

    redirect implemented

--- express_server.js
+++ express_server.js
@@ -53,9 +53,24 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const { longURL } = req.body;
+  const shortURL = generateRandomString(6);
+  // console.log(shortURL, longURL);
+  urlDatabase[shortURL] = longURL;
+  // console.log(urlDatabase);
+
+  const templateVars = {
+    shortURL,
+    longURL,
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
